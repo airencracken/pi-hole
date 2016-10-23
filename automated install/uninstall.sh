6ce72fa50bc27cbd3cdf9d425d454d256ea763ec
Marcus Hildum <marcus.hildum@gmail.com>
2016-10-23 16:39:40 -0700

    Missed a couple of variables

--- automated install/uninstall.sh
+++ automated install/uninstall.sh
@@ -10,6 +10,8 @@
 # the Free Software Foundation, either version 2 of the License, or
 # (at your option) any later version.
 
+source spinner.sh
+
 # Must be root to uninstall
 if [[ ${EUID} -eq 0 ]]; then
 	echo "::: You are root."
